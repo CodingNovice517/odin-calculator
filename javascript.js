869e05e691089364581aa517a2e35a470fc5b988
CodingNovice517 <dachad517@gmail.com>
2025-08-25 19:23:19 -0500

    Created operator function and variables to handle user inputs

--- javascript.js
+++ javascript.js
@@ -1,3 +1,13 @@
+// Global variables to store user input
+let userInput = null;  // The input string to be parsed
+let curResult = null;  // To store the result of the latest operation
+
+// Variables mainly used to perform an operation
+let userNum1 = null;
+let userNum2 = null;
+let userOperator = null;
+
+
 // Basic Calculator Functions
 function add(num1, num2) {
     return num1 + num2;
@@ -13,4 +23,19 @@ function multiply(num1, num2) {
 
 function divide(num1, num2) {
     return num1 / num2;
+}
+
+function operate(num1, num2, operator) {
+    switch (operator) {
+        case "+":
+            return add(num1, num2);
+        case "-":
+            return subtract(num1, num2);
+        case "*":
+            return multiply(num1, num2);
+        case "/":
+            return divide(num1, num2);
+        default:
+            return null;
+    }
 }
